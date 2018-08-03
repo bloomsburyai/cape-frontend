48ca9702eaa059f84644c35d5bcd5f32dd163ca3
Luis Ulloa <timken@gmail.com>
2018-08-03 15:15:49 +0100

    Dynamic testing URL

--- cape_frontend/webapp/tests/tests_settings.py
+++ cape_frontend/webapp/tests/tests_settings.py
@@ -1,2 +1,4 @@
-UI_URL = 'http://localhost:5050'
+import os
+from cape_frontend.cape_frontend_settings import CONFIG_SERVER
 
+UI_URL = os.getenv('CAPE_FRONTEND_TEST_URL', f'http://localhost:{CONFIG_SERVER["port"]}')
